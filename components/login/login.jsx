29546dca52ba98212d5c91cb6234ad7d6f4834f8
REL1980 <rafaelandeau@hotmail.com>
2022-07-20 10:47:01 -0300

    commit

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <p>aca vamos a hacer el login (lucas estuvo aqui, kevin igual, y Hernán tambien!)</p>
+      <p>ok!! aca vamos a hacer el login (lucas estuvo aqui, kevin igual, y Hernán tambien!)</p>
       <button>entra!</button>
     </>
   );
