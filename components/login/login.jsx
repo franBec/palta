b658efff774d473f720cf485f17d6d5a5bcd9497
Fran Bec <81979771+franBec@users.noreply.github.com>
2022-07-20 10:47:58 -0300

    se agrego pollito

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <p>ok!! aca vamos a hacer el login (lucas estuvo aqui, kevin igual, y Hernán tambien!)</p>
+      <p>
+        ok!! aca vamos a hacer el login (lucas estuvo aqui, kevin igual, y
+        Hernán tambien!)
+      </p>
+      <p>🐤!</p>
       <button>entra!</button>
     </>
   );
