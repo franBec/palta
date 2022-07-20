515b201e396ec8f35d314b6fb3bd934189ec5eae
ElHernan14 <42650629+ElHernan14@users.noreply.github.com>
2022-07-20 10:37:23 -0300

    Hernaaan, estuvo en palta

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <p>aca vamos a hacer el login (lucas estuvo aqui, kevin igual)</p>
+      <p>aca vamos a hacer el login (lucas estuvo aqui, kevin igual, y Hernán tambien!)</p>
       <button>entra!</button>
     </>
   );
