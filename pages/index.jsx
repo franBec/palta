784bb7963870c75f13be69b2dff37d30521b733b
Fran Bec <81979771+franBec@users.noreply.github.com>
2022-07-28 09:42:49 -0300

    mejoras en zustand store e index principal

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { GiAvocado } from "react-icons/gi";
 import useSWR from "swr";
 import Router from "next/router";
@@ -6,29 +5,29 @@ import LoginComponent from "../components/login/loginComponent"
 import { useCurrentUser } from '../zustand/SessionStore';
 
 const Login = () => {
+
+  //*obtengo setters de la sessionStore de Zustand
   const setUser = useCurrentUser((state) => state.set_CurrentUser)
   const setPermisos = useCurrentUser((state) => state.set_permisosCurrentUser)
-  const [stateUser, setStateUser] = useState()
-  const [statePermisos, setStatePermisos] = useState()
 
-  useEffect(() => {
-    setStateUser(setUser)
-    setStatePermisos(setPermisos)
-  },[setUser,setPermisos])
-
+  //*fetcher de swr
   const fetchLogin = async (url) => {
     const res = await fetch(url);
     const resjson = await res.json();
     return resjson;
   };
 
+  //*swr
   const { data, error, mutate } = useSWR(
     "/api/user/user",
     fetchLogin
   );
 
+  //*action del boton login
   const login = async ({usuario,password}) => {
     try {
+
+      //llamada al backend pora verificar credenciales
       const res = await fetch("api/auth", {
         method: "POST",
         headers: { "Content-type": "application/json" },
@@ -39,20 +38,32 @@ const Login = () => {
         }),
       });
       const resFromBackend = await res.json();
+
+
       if(!resFromBackend.success){
-        console.log(resFromBackend);
-      }else{
+        //!credenciales invalidas
+        alert('Credenciales incorrectas');  
+      }
+      else{
+
+        //agrego a la sessionStore de zustand los datos recien obtenidos
         setUser(resFromBackend.data)
         setPermisos(resFromBackend.permisos)
-        //Muto el usuario así me re-renderiza el componente
+
+        //refresco data declarada en swr
+        //Ahora se supone que al hacer fetch, data.loggedIn debe volver true
         mutate("/api/user/user")
+
       }
     } catch (error) {
-      console.log(error);
+      alert(error.message);
     }
   };
 
+  //*renderizado
   const renderMainContent = () => {
+    
+    //cargando....
     if (!data) {
       return (
         <div role="status">
@@ -63,10 +74,12 @@ const Login = () => {
       );
     }
     
+    //algo salio mal
     if (error) {
       return <div>failed to load</div>;
     }
 
+    //verificamos si debemos quedarnos en el log in o movernos a main menu
     if(data?.isLoggedIn){
       Router.push("/mainMenu")
     }else{
@@ -74,11 +87,8 @@ const Login = () => {
     }
   };
 
-  return (
-    <>
-      {renderMainContent()}
-    </>
-  );
+  //*return
+  return (renderMainContent());
 };
 
 export default Login;
